844e8343560673ca5f3e3ae2eb4b6fb2fa3ead90
alex <rojkov@gmail.com>
2015-11-17 11:02:56 -0800

    initial commit

---
+++ src/main/java/web/HelloServiceImpl.java
@@ -0,0 +1,18 @@
+package web;
+
+import io.baratine.core.Service;
+
+/**
+ * The service is deployed into web app context and is available for rest calls
+ * via curl 'http://localhost{:port}/{context}/jamp/hello?m=hello&p0=World'
+ *
+ * e.g. curl 'http://localhost:8080/hello/jamp/hello?m=hello&p0=World'
+ */
+@Service("public:///hello")
+public class HelloServiceImpl
+{
+  public String hello(String arg)
+  {
+    return "Hello " + arg;
+  }
+}
